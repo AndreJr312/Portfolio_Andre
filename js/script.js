f661249f38b07eac8e0205ae492e3490b69aedee
Andre <cardoso.andrejunior@gmail.com>
2022-08-21 23:45:04 -0300

    adicionando animação ao rodar a pagina e ajustes de estilos nas divs de projeto

--- js/script.js
+++ js/script.js
@@ -5,18 +5,21 @@ const nome = document.getElementById('nome_escrito')
 const text = 'André Luis'
 const intervalo = 150
 
+const target = document.querySelectorAll("[data-anime]")
+const animationClass = "animate"
+
+
+//função para animar menu hamburguer mobile
+
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefalt() // previnindo o mobile de dar um clipe duplo ao abrir o menu
     const nav = document.getElementById('navegar')
     nav.classList.toggle('active')
-
 }
 
 btnMobile.addEventListener('click', toggleMenu)
 btnMobile.addEventListener('touchstart', toggleMenu)  //para mobile
-
 itemLista.addEventListener('click', toggleMenu)
-
 
 
 //função para escrever nome no inicio
@@ -37,5 +40,22 @@ function showText (nome, text, intervalo){
 
     }, intervalo)
 }
-
 showText(nome, text, intervalo)
+
+//função para animar itens ao rolar a tela
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.70)
+    target.forEach(function(e){
+        if((windowTop) > e.offsetTop){
+            e.classList.add(animationClass)
+        }else{
+            e.classList.remove(animationClass)
+        }
+    })
+}
+animeScroll()
+
+window.addEventListener("scroll", function(){
+    animeScroll()
+})
